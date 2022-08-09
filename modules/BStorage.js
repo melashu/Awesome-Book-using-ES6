efb7337f0f9d10ba6fbb3ab69f0c875046087224
melashu <meshu102@outlook.com>
2022-08-09 15:46:43 +0300

    filter books by Id

--- modules/BStorage.js
+++ modules/BStorage.js
@@ -1,8 +1,8 @@
 export default class BStorage {
   static getBook = () => {
     let book = [];
-    if (localStorage.getItem('ourbook') !== null) {
-      book = JSON.parse(localStorage.getItem('ourbook'));
+    if (localStorage.getItem("ourbook") !== null) {
+      book = JSON.parse(localStorage.getItem("ourbook"));
     } else {
       book = [];
     }
@@ -12,18 +12,17 @@ export default class BStorage {
   static addBook = (book) => {
     const bookList = BStorage.getBook();
     bookList.push(book);
-    localStorage.setItem('ourbook', JSON.stringify(bookList));
+    localStorage.setItem("ourbook", JSON.stringify(bookList));
   };
 
   static removeBook = (e) => {
-    if (e.target.classList.contains('btn-remove')) {
+    if (e.target.classList.contains("btn-remove")) {
       let list = BStorage.getBook();
       const parent = e.target.parentElement.parentElement;
-      const elementIndex = e.target.id;
+      const elementIndex = e.target;
       parent.remove();
-      const result = list.filter((book) => book !== list[elementIndex]);
-      list = result;
-      localStorage.setItem('ourbook', JSON.stringify(result));
+      let result = list.filter((book) => book.id != elementIndex.id);
+      localStorage.setItem("ourbook", JSON.stringify(result));
     }
   };
 }
