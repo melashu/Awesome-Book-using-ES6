9dc3383a0ff4ea1ebdb92aabd2cefa3624b2751e
melashu <meshu102@outlook.com>
2022-08-09 11:03:00 +0300

    Update current date object

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-import { Book } from "./modules/Book.js";
-import { BStorage } from "./modules/BStorage.js";
-import { UI } from "./modules/UI.js";
+import Book from './modules/Book.js';
+import BStorage from './modules/BStorage.js';
+import UI from './modules/UI.js';
 
-document.querySelector("form").addEventListener("submit", (event) => {
+document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault();
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
 
   const book = new Book(title, author);
 
   UI.addBookToScreen(book, BStorage.getBook().length);
   BStorage.addBook(book);
-  document.getElementById("title").value = "";
-  document.getElementById("author").value = "";
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
 });
 
-document.addEventListener("DOMContentLoaded", UI.displayBook);
+document.addEventListener('DOMContentLoaded', UI.displayBook);
 
 document
-  .getElementsByClassName("book-list")[0]
-  .addEventListener("click", BStorage.removeBook);
+  .getElementsByClassName('book-list')[0]
+  .addEventListener('click', BStorage.removeBook);
 
-document.querySelector(".list").addEventListener("click", () => {
-  document.getElementsByClassName("book")[0].style.display = "block";
-  UI.getBook().style.display = "none";
-  document.getElementsByClassName("contact-me")[0].style.display = "none";
+document.querySelector('.list').addEventListener('click', () => {
+  document.getElementsByClassName('book')[0].style.display = 'block';
+  UI.getBook().style.display = 'none';
+  document.getElementsByClassName('contact-me')[0].style.display = 'none';
 });
 
-document.querySelector(".add").addEventListener("click", (e) => {
+document.querySelector('.add').addEventListener('click', (e) => {
   e.preventDefault();
-  document.getElementsByClassName("book")[0].style.display = "none";
-  document.getElementsByClassName("contact-me")[0].style.display = "none";
-  UI.getBook().style.display = "flex";
+  document.getElementsByClassName('book')[0].style.display = 'none';
+  document.getElementsByClassName('contact-me')[0].style.display = 'none';
+  UI.getBook().style.display = 'flex';
 });
 
-document.querySelector(".contact").addEventListener("click", (e) => {
+document.querySelector('.contact').addEventListener('click', (e) => {
   e.preventDefault();
-  document.getElementsByClassName("book")[0].style.display = "none";
-  document.getElementsByClassName("contact-me")[0].style.display = "flex";
-  document.getElementsByClassName("add-book-list")[0].style.display = "none";
+  document.getElementsByClassName('book')[0].style.display = 'none';
+  document.getElementsByClassName('contact-me')[0].style.display = 'flex';
+  document.getElementsByClassName('add-book-list')[0].style.display = 'none';
 });
